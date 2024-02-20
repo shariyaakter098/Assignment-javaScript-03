26f3132c175d4df77a5ce1dd9880591e08d00cb7
Shariya Akter <shariyaakter098@gmail.com>
2024-02-20 16:06:16 +0600

    first commit

---
+++ first.js
@@ -0,0 +1,31 @@
+
+
+let colors = "red green black white";
+{
+    color = "red" ;
+}
+
+
+const color = ["red", "green", "black", "white"];
+document.getElementById("name").innerHTML = color;
+
+color. push("pink")
+
+color. pop("red")
+
+console.log(color.length);
+
+
+
+
+
+var FirstColor = "red";
+
+var FifstColor = "black";
+
+if (FifstName < LastName){
+    console.log("Give me a Doll");
+}
+else{
+    console.log("Give me a Boll ");
+}
